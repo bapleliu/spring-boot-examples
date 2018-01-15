12b1ffb9ad222e1bf9470e0e60c33389a56f204c
lbp <qdliubp@126.com>
2018-01-15 16:25:11 +0800

    代码优化

--- spring-boot-mail/src/test/java/com/neo/ApplicationTests.java
+++ spring-boot-mail/src/test/java/com/neo/ApplicationTests.java
@@ -1,17 +1,51 @@
 package com.neo;
 
+import com.neo.service.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		System.out.println("hello world");
-	}
+
+    @Autowired
+    private MailService mailService;
+    private static final String[] RECIPIENTS = {"874822608@qq.com"};
+    private static final String SUBJECT = "proba";
+
+    @Test
+    public void contextLoads() {
+        System.out.println("hello world");
+    }
+
+    @Test
+    public void sendNormalTextMessage() {
+        try {
+            mailService.sendSimpleMail("874822608@qq.com", SUBJECT, "Ez egy lev�l t�rzs");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Test
+    public void sendHtmlTemplateMessage() {
+        String[] messages = {"Ez egy �zenet�������", "Valami", "m�sik", "Harmadik"};
+        Map<String, Object> datas = new HashMap<>();
+        datas.put("messages", messages);
+        try {
+//            mailService.sendHtmlMail("874822608@qq.com", SUBJECT, "MailTemplate1");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 
 }
